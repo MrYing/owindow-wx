983aa287db317c1e67326a2a08f3503e0327dafc
steven <steven@peipeiyun.com>
2017-07-17 23:20:55 +0800

    canvas
    添加canvas页面

--- pages/API/canvas/canvas.js
+++ pages/API/canvas/canvas.js
@@ -12,7 +12,35 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.context = wx.createContext()
+    var methods = Object.keys(example)
+    this.setData({
+      methods:methods
+    })
+    var that = this
+    methods.forEach(function(method){
+      that[method] = function(){
+        example[method](that.context)
+        var actions = that.context.getActions()
+
+        wx.drawCanvas({
+          canvasId:'canvas',
+          actions:actions
+        })
+      }
+    })
+  },
+
+  toTempFilePath:function(){
+    wx.toTempFilePath({
+      canvasId:'canvas',
+      success:function(res){
+        console.log(res)
+      },
+      fail:function(res){
+        console.log(res)
+      }
+    })
   },
 
   /**
