aed2affd16b9cc62b104727eab0257067f449afc
steven <steven@peipeiyun.com>
2017-07-20 14:05:08 +0800

    music
    音乐播放可以出声音，不用链接删除

--- pages/music/index.js
+++ pages/music/index.js
@@ -8,7 +8,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    songRec:[]
+    songRec:[],
+    isPlay:"none"
   },
 
   /**
--- pages/player/player.js
+++ pages/player/player.js
@@ -11,7 +11,8 @@ Page({
    ctrl:'音乐控制区域',
    cpTime:'00:00',
    duration:'00:00',
-   lrcList:[]
+   lrcList:[],
+   audioSrc: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
   },
 
   /**
